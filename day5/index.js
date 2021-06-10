1b379a3e6dee2540644f1d179b97d8f3892b9294
aguyran <59661860+aguyran@users.noreply.github.com>
2021-06-10 21:28:47 +0530

    added day 5

---
+++ day5/index.js
@@ -0,0 +1,21 @@
+window.addEventListener('load',()=>{
+    const calculate =(obj)=> {
+        let x = parseFloat(document.getElementById("oneTxt").value);
+        let y = parseFloat(document.getElementById("secondTxt").value);
+        switch(obj){
+            case "add":
+                return x+y;
+            case "sub":
+                return x-y;
+            case "mul":
+                return x*y;
+            case "div":
+                return x/y;
+            default: return "invalid case";
+        }
+    }
+    const buttons = document.getElementById("buttonGroup");
+    buttons.addEventListener('click',(evt)=>{
+       document.getElementById("result").innerHTML=calculate(evt.target.id);
+    });
+});
