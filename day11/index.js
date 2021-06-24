439561eaa4955973dbba62864b3c6f1dd792df57
aguyran <59661860+aguyran@users.noreply.github.com>
2021-06-24 21:39:34 +0530

    alpha

--- day11/index.js
+++ day11/index.js
@@ -1,58 +1,105 @@
-let movies = null;
+let questions = null;
+let globalQues = 0;
+let score = 0;
+let mergedArray = [];
 const startBtn = document.getElementById("start");
 const mainDiv = document.getElementById("main");
 const container = document.getElementById("container");
-startBtn.addEventListener("click", () => random());
-async function random() {
-  const response = await fetch(
-    "https://opentdb.com/api.php?amount=10&difficulty=medium"
+const quesContainerMain = document.getElementById("quesContainer");
+const scoreLabel = document.getElementById("score");
+startBtn.addEventListener("click", () => {
+  fetch("https://opentdb.com/api.php?amount=10&difficulty=medium").then(
+    function (response) {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      console.log(response.status);
+      response.json().then(function (response) {
+        if (!response["response_code"]) {
+          questions = response["results"];
+          startQuiz();
+        } else console.log("error! Fetching");
+      });
+    }
   );
-  movies = await response.json();
-  if (movies["response_code"] === 0) {
-    console.log(movies);
-    movies = movies["results"];
-    startQuiz();
-    return "Success";
-  } else {
-    alert("Error Fetching Questions! Please retry after sometime");
-    return "Failed";
-  }
-}
+});
 
 function startQuiz() {
   mainDiv.classList.remove("main-menu");
   mainDiv.classList.add("game-canvas");
   startBtn.classList.add("disabled");
+  document.getElementsByTagName("form")[0].classList.add("disabled");
+  quesContainerMain.classList.remove("disabled");
   container.classList.remove("disabled");
   container.classList.add("game-container");
-  container.appendChild(quesObj(1));
+  scoreLabel.classList.remove("disabled");
+  quesContainerMain.appendChild(quesObj(globalQues));
 }
 
 function quesObj(quesNum) {
   let quesContainer = document.createElement("div");
+  mergedArray = [
+    ...questions[quesNum]["incorrect_answers"],
+    questions[quesNum]["correct_answer"],
+  ];
+  console.log(mergedArray);
+  shuffle(mergedArray);
   let ques = document.createElement("h1");
   ques.className = "heading";
-  ques.innerHTML = movies[quesNum]["question"];
+  ques.innerHTML = questions[quesNum]["question"];
   quesContainer.appendChild(ques);
 
-  if (movies[quesNum]["type"] === "boolean") {
-    quesContainer.appendChild(optionObject(2));
-  } else if (movies[quesNum]["type"] === "multiple") {
-    quesContainer.appendChild(optionObject(4));
+  if (questions[quesNum]["type"] === "boolean") {
+    quesContainer.appendChild(optionObject(2, mergedArray));
+  } else if (questions[quesNum]["type"] === "multiple") {
+    quesContainer.appendChild(optionObject(4, mergedArray));
   }
   return quesContainer;
 }
 
-function optionObject(numberOfOptions) {
+function optionObject(numberOfOptions, mergedArray) {
   let optionDiv = document.createElement("div");
   optionDiv.className = "options";
   for (let i = 0; i < numberOfOptions; i++) {
     let optionName = document.createElement("p");
     let optionButton = document.createElement("button");
     optionName.className = "option-name";
+    optionName.innerText = mergedArray[i];
     optionButton.className = "ans-btn";
     optionDiv.appendChild(optionName);
     optionDiv.appendChild(optionButton);
+    optionButton.innerText = "Click Here";
+    optionButton.addEventListener("click", () => forButton(i));
   }
+
   return optionDiv;
+}
+
+// Fishers Algorithm From
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+function forButton(i) {
+  if (mergedArray[i] === questions[globalQues]["correct_answer"]) {
+    score += 1;
+  }
+  globalQues += 1;
+  quesContainerMain.removeChild(quesContainerMain.childNodes[0]);
+  quesContainerMain.appendChild(quesObj(globalQues));
+  scoreLabel.innerText = `Score: ${score}`;
 }
