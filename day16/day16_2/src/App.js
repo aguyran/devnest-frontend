88a25e3ed5b1fb68d5e69009974a61bf18ed3961
aguyran <59661860+aguyran@users.noreply.github.com>
2021-07-02 19:17:19 +0530

    day 16 completed

--- day16/day16_2/src/App.js
+++ day16/day16_2/src/App.js
@@ -1,19 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
 const array2 = [];
-for (let i = 0; i < 100; i++) {
-  array2.push(i);
+for (let i = 0; i < 10; i++) {
+  array2.push([]);
+  for (let j = 0; j < 10; j++) {
+    array2[i].push(j);
+  }
 }
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <div className="board">
-        {array2.map((i) => (
-          <div className="test" key={i}>
-            {i}
-          </div>
-        ))}
+        {array2.map((i, index) => {
+          return array2[index].map((j, index2) => {
+            if (index === 0 || index2 === 9 || index2 === 0 || index === 9) {
+              return <div className="black" key={i}></div>;
+            } else if (index % 2 !== 0) {
+              if (index2 % 2 !== 0)
+                return <div className="black" key={i}></div>;
+              else return <div className="test" key={i}></div>;
+            } else if (index % 2 === 0) {
+              if (index2 % 2 === 0)
+                return <div className="black" key={i}></div>;
+              else return <div className="test" key={i}></div>;
+            }
+          });
+        })}
       </div>
     </div>
   );
