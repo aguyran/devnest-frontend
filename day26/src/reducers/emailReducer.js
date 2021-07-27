ce234ecbaf3f66abc2f92677170c6c6500681b97
aguyran <59661860+aguyran@users.noreply.github.com>
2021-07-27 21:43:32 +0530

    changes

--- day26/src/reducers/emailReducer.js
+++ day26/src/reducers/emailReducer.js
@@ -1,7 +1,5 @@
 const emailReducer = (state = "", action) => {
   if (action.type === "TEXT_WRITTEN") {
-    console.log(action.text);
-    state = action.text;
     return state;
   }
   return state;
